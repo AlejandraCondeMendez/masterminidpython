ca05f93cfa8aca78fd4eb11cc8e0f2bf3bc08fc6
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-08-05 09:20:45 -0600

    Estableciendo la clase tablero

--- tablero.py
+++ tablero.py
@@ -1,1 +1,20 @@
-from colored import fg
+from colored import fg, attr
+
+class Tablero:
+    colores={
+        "rojo": fg("red"),
+        "azul": fg("blue"),
+        "verde": fg("green"),
+        "amarillo": fg("yellow")
+    }
+    
+    def __init__(self):
+        self.color=[]
+        self.turnos=[]
+    
+    
+    def definir_color(self, color):
+        self.color = color #Lista que contiene los colores
+    
+    
+        
